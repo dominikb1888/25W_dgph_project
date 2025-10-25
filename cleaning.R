41ef588330690544d3528e3afc34d89efe183918
Dominik Böhler <dominik.boehler@gmx.net>
2025-10-25 17:05:41 +0200

    after session 2

---
+++ cleaning.R
@@ -0,0 +1,28 @@
+pacman::p_load(
+  rio,        # importing data  
+  here,       # relative file pathways  
+  janitor,    # data cleaning and tables
+  lubridate,  # working with dates
+  matchmaker, # dictionary-based cleaning
+  epikit,     # age_categories() function
+  tidyverse,   # data management and visualization
+  skimr
+)
+
+# IMPORT ---------------------------------------
+linelist_raw <- import("linelist_raw.xlsx")
+#skimr::skim(linelist_raw)
+
+
+# CLEANING -----------------------------------
+linelist <- linelist_raw %>% 
+
+  ## Clean Column Names ----------------------
+  janitor::clean_names() %>%
+
+  ## Rename Column Names ---------------------
+  rename(date_infection       = infection_date,
+         date_hospitalisation = hosp_date,
+         date_outcome         = date_of_outcome) %>%
+  
+  distinct()
